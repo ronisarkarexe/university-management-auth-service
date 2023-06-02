1eb2c7498471ced73dc531bff2ad5d5f479bd501
Roni Chandra Sarkar <ronichandrasarkar@gmail.com>
2023-06-02 15:40:03 +0530

    some modification and create logs folder

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,13 +1,34 @@
 import winston from 'winston'
+import path from 'path'
 
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
+    new winston.transports.File({
+      filename: path.join(process.cwd(), 'logs', 'winston', 'success.log'),
+      level: 'info',
+    }),
   ],
 })
 
-export { logger }
+const errorLogger = winston.createLogger({
+  level: 'error',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: path.join(process.cwd(), 'logs', 'winston', 'error.log'),
+      level: 'error',
+    }),
+  ],
+})
+
+export { logger, errorLogger }
+
+/*
+logs/winston
+success.log
+error.log
+*/
