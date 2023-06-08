8b862c83b5fbff23fd92fecc4bcd455acbf6ca7c
Roni Chandra Sarkar <ronichandrasarkar@gmail.com>
2023-06-09 00:11:11 +0530

    Api not found

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from 'express'
+import express, { Application, NextFunction, Request, Response } from 'express'
 import cors from 'cors'
 const app: Application = express()
 
@@ -11,10 +11,25 @@ app.use(express.urlencoded({ extended: true }))
 
 import globalErrorHandler from './app/middlewares/globalErrorHandler'
 import routers from './app/routes'
+import httpStatus from 'http-status'
 
 // Application router
 app.use('/api/v1', routers)
 
 app.use(globalErrorHandler)
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found',
+    errorMessages: [
+      {
+        path: '.',
+        message: 'Api Not Found',
+      },
+    ],
+  })
+  next()
+})
 
 export default app
